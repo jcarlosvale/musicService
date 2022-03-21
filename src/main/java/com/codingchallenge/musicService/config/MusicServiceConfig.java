96c539aca36d25146e68040593d7d3196a7de88c
Joao Carlos Sousa do Vale <joaocarlos.sousadovale@tipico.com>
2022-03-21 17:35:12 +0100

    inserting cache

--- src/main/java/com/codingchallenge/musicService/config/MusicServiceConfig.java
+++ src/main/java/com/codingchallenge/musicService/config/MusicServiceConfig.java
@@ -1,5 +1,9 @@
 package com.codingchallenge.musicService.config;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
+import java.util.concurrent.TimeUnit;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -10,5 +14,19 @@ public class MusicServiceConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public Caffeine caffeineConfig() {
+        return Caffeine
+                .newBuilder()
+                .expireAfterWrite(60, TimeUnit.MINUTES);
+    }
+
+    @Bean
+    public CacheManager cacheManager(final Caffeine caffeine) {
+        CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager();
+        caffeineCacheManager.setCaffeine(caffeine);
+        return caffeineCacheManager;
     }
 }
